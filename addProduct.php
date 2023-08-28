e7a067d7467bbbf5f5a0455c20ed234a7989784a
adzazarif <adzazarf@gmail.com>
2023-08-28 09:38:40 +0700

    seconf commit

--- addProduct.php
+++ addProduct.php
@@ -2,7 +2,7 @@
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Add Product</h1>
-        <a class="btn btn-primary" href="tambahBarang.php" role="button">Tambah</a>
+        <a class="btn btn-primary" href="tambahBarang.php" role="button">add</a>
       </div>
 
       <form action="" method="POST">
